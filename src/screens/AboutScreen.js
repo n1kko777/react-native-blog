8ec5feecf7b04aa0ffe46aac7fe49a781f6d25c6
n1kko777 <3734489@gmail.com>
2020-02-02 21:00:00 +0500

    Завершение навигации

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,19 +1,36 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Text } from "react-native-elements";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { AppHeaderIcons } from "../components/AppHeaderIcons";
 
 export const AboutScreen = () => {
-  const { center } = styles;
+  const { container } = styles;
   return (
-    <View style={center}>
-      <Text>AboutScreen</Text>
+    <View style={container}>
+      <Text style={{ fontFamily: "open-semibold" }} h2>
+        Личный блог
+      </Text>
+      <Text style={{ marginTop: 20, fontFamily: "open-regular" }}>
+        Версия приложения:{" "}
+        <Text style={{ fontFamily: "open-semibold" }}>1.0.0</Text>
+      </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  center: { flex: 1, justifyContent: "center", alignItems: "center" }
+  container: {
+    marginHorizontal: 10,
+    marginTop: 10,
+    marginBottom: 30,
+    paddingTop: 15,
+    paddingBottom: 25,
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
 
 AboutScreen.navigationOptions = ({ navigation }) => ({
