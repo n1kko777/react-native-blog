5c863634c8b642199617db2491427f62601ac24d
n1kko777 <3734489@gmail.com>
2020-02-02 18:23:02 +0500

    Отфильтрованные карточки

--- src/screens/BookMarkedScreen.js
+++ src/screens/BookMarkedScreen.js
@@ -1,15 +1,41 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { FlatList, View } from "react-native";
+import { withTheme } from "react-native-elements";
 
-export const BookMarkedScreen = () => {
-  const { center } = styles;
-  return (
-    <View style={center}>
-      <Text>BookMarkedScreen</Text>
-    </View>
+import { DATA } from "../data";
+import { CustomCard } from "../components/CustomCard";
+
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { AppHeaderIcons } from "../components/AppHeaderIcons";
+
+export const BookMarkedScreen = withTheme(({ navigation, theme }) => {
+  const keyExtractor = item => item.id.toString();
+
+  const renderItem = ({ item }) => (
+    <CustomCard item={item} theme={theme} navigation={navigation} />
   );
-};
 
-const styles = StyleSheet.create({
-  center: { flex: 1, justifyContent: "center", alignItems: "center" }
+  return (
+    <FlatList
+      ListFooterComponent={<View></View>}
+      ListFooterComponentStyle={{ paddingBottom: 30 }}
+      keyExtractor={keyExtractor}
+      data={DATA.filter(elem => elem.booked)}
+      renderItem={renderItem}
+    />
+  );
 });
+
+BookMarkedScreen.navigationOptions = {
+  title: "Избранные",
+  headerTitle: "Избранные",
+  headerLeft: () => (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcons}>
+      <Item
+        title="Toggle Drawer"
+        iconName="ios-menu"
+        onPress={() => console.log("Toggle Drawer")}
+      />
+    </HeaderButtons>
+  )
+};
