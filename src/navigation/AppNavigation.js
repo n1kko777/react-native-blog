aefef5dcf6a32d684cca5d1aebde8782b53f2a97
n1kko777 <3734489@gmail.com>
2020-02-02 18:44:22 +0500

    Адаптация меню под андроид

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -11,6 +11,8 @@ import { MainSreen } from "../screens/MainSreen";
 import { PostScreen } from "../screens/PostScreen";
 import { BookMarkedScreen } from "../screens/BookMarkedScreen";
 
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+
 import { THEME } from "../theme";
 
 const PostNavigator = createStackNavigator(
@@ -48,30 +50,40 @@ const BookedNavigator = createStackNavigator(
   }
 );
 
-const BottomNavogator = createBottomTabNavigator(
-  {
-    Post: {
-      screen: PostNavigator,
-      navigationOptions: {
-        tabBarIcon: info => (
-          <Ionicons name="ios-albums" size={25} color={info.tintColor} />
-        )
-      }
-    },
-    Booked: {
-      screen: BookedNavigator,
-      navigationOptions: {
-        tabBarIcon: info => (
-          <Ionicons name="ios-star" size={25} color={info.tintColor} />
-        )
-      }
+const bottomTabsConfig = {
+  Post: {
+    screen: PostNavigator,
+    navigationOptions: {
+      tabBarLabel: "Все",
+      tabBarIcon: info => (
+        <Ionicons name="ios-albums" size={25} color={info.tintColor} />
+      )
     }
   },
-  {
-    tabBarOptions: {
-      activeTintColor: THEME.MAIN_COLOR
+  Booked: {
+    screen: BookedNavigator,
+    navigationOptions: {
+      tabBarLabel: "Избранное",
+      tabBarIcon: info => (
+        <Ionicons name="ios-star" size={25} color={info.tintColor} />
+      )
     }
   }
-);
+};
+
+const BottomNavogator =
+  Platform.OS === "ios"
+    ? createBottomTabNavigator(bottomTabsConfig, {
+        tabBarOptions: {
+          activeTintColor: THEME.MAIN_COLOR
+        }
+      })
+    : createMaterialBottomTabNavigator(bottomTabsConfig, {
+        activeTintColor: "#fff",
+        shifting: true,
+        barStyleLight: {
+          backgroundColor: THEME.MAIN_COLOR
+        }
+      });
 
 export const AppNavigation = createAppContainer(BottomNavogator);
