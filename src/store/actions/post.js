8e7308c19cd1a8e15bd65b30d3776c47eae4f903
n1kko777 <3734489@gmail.com>
2020-02-03 22:18:47 +0500

    Получение данных из БД

--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -1,9 +1,14 @@
 import { LOAD_POSTS, TOGGLE_BOOKED, REMOVE_POST, ADD_POST } from "../types";
+import { DB } from "../../db";
 
-export const loadPosts = () => ({ type: LOAD_POSTS, payload: [] });
+export const loadPosts = () => async dispatch =>
+  dispatch({ type: LOAD_POSTS, payload: await DB.getPosts() });
 
-export const toggleBooked = id => ({ type: TOGGLE_BOOKED, payload: id });
+export const toggleBooked = id => disptch =>
+  disptch({ type: TOGGLE_BOOKED, payload: id });
 
-export const removePost = id => ({ type: REMOVE_POST, payload: id });
+export const removePost = id => dispatch =>
+  dispatch({ type: REMOVE_POST, payload: id });
 
-export const addPost = post => ({ type: ADD_POST, payload: post });
+export const addPost = post => dispatch =>
+  dispatch({ type: ADD_POST, payload: post });
